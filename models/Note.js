7cc2ad517db5a282e88b8caba76274d3a584d7c4
davebattles <davidoflores@gmail.com>
2018-06-18 00:09:04 -0500

    running, need to style handlebars

--- models/Note.js
+++ models/Note.js
@@ -12,7 +12,7 @@ var Schema = mongoose.Schema;
 
 var noteSchema = new Schema({
   _headlineId: {
-    type: Schema.types.ObjectId
+    type: Schema.Types.ObjectId
   },
   date: String,
   noteText: String
